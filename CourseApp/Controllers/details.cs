20ac7a089055521574eaff2b2eb76c48020b9ebc
Farukaksungur <farukaksungur.ceng@gmail.com>
2021-02-02 14:00:31 +0300

    two

---
+++ CourseApp/Controllers/details.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    public class details : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
